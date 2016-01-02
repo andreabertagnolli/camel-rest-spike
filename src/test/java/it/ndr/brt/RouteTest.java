d54e2b6272cd058e6b9d84d8a4a6485c8c22e2d1
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2016-01-02 01:39:27 +0100

    first scaffolding

---
+++ src/test/java/it/ndr/brt/RouteTest.java
@@ -0,0 +1,22 @@
+package it.ndr.brt;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class RouteTest {
+
+    @Ignore
+    @Test
+    public void test() {
+        Response response = ClientBuilder.newClient().target("http://localhost:8080/upload").request().post(Entity.entity("entity", MediaType.TEXT_PLAIN_TYPE));
+
+        Assert.assertEquals(200, response.getStatus());
+    }
+}
