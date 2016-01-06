bcdbb5e171f473fa2c6a4932042819c9013fd912
andrea.bertagnolli <andrea.bertagnolli@gmail.com>
2016-01-06 11:01:16 +0100

    refactoring test

--- src/test/java/it/ndr/brt/RestApplicationTest.java
+++ src/test/java/it/ndr/brt/RestApplicationTest.java
@@ -1,20 +1,25 @@
 package it.ndr.brt;
 
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
+
 import org.apache.camel.CamelContext;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RestApplicationTest {
 
+	@Mock private CamelContext context;
+	
     @Test
     public void application_building() throws Exception {
-        CamelContext context = Mockito.mock(CamelContext.class);
+        new RestApplication(context).start();
 
-        RestApplication spike = new RestApplication(context);
-        spike.start();
-
-        Mockito.verify(context).addRoutes(Mockito.isA(RestRouteBuilder.class));
-        Mockito.verify(context).addRoutes(Mockito.isA(UploadRouteBuilder.class));
-        Mockito.verify(context).start();
+        verify(context).addRoutes(isA(RestRouteBuilder.class));
+        verify(context).addRoutes(isA(UploadRouteBuilder.class));
+        verify(context).start();
     }
 }
