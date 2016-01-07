1d8bc2f2d92f097ac495920268386df1595aff62
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2016-01-07 15:58:40 +0100

    adding camel main to Application

--- src/main/java/it/ndr/brt/RestApplication.java
+++ src/main/java/it/ndr/brt/RestApplication.java
@@ -2,10 +2,19 @@ package it.ndr.brt;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
 
 public class RestApplication {
 
     private final CamelContext context;
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.enableHangupSupport();
+        main.addRouteBuilder(new RestRouteBuilder());
+        main.addRouteBuilder(new UploadRouteBuilder());
+        main.run();
+    }
 
     public RestApplication() {
         this.context = new DefaultCamelContext();
